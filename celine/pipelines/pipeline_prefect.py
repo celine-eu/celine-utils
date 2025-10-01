4e84f0a64729e23eb95195f97516ac6f0c8d2417
lc-spxl <luca.capra@spindox.it>
2025-10-01 12:46:50 +0200

    fix config parsing

--- celine/pipelines/pipeline_prefect.py
+++ celine/pipelines/pipeline_prefect.py
@@ -3,37 +3,43 @@ from celine.pipelines.pipeline_config import PipelineConfig
 from celine.pipelines.pipeline_runner import PipelineRunner
 
 
-def meltano_run(command: str = "run import", cfg: dict = {}) -> Dict[str, Any]:
+def _get_config(cfg: dict | PipelineConfig) -> PipelineConfig:
+    return cfg if isinstance(cfg, PipelineConfig) else PipelineConfig(**cfg)
+
+
+def meltano_run(
+    command: str = "run import", cfg: dict | PipelineConfig = {}
+) -> Dict[str, Any]:
     """
     Prefect task wrapper for PipelineRunner.run_meltano.
     """
-    runner = PipelineRunner(PipelineConfig(**cfg))
+    runner = PipelineRunner(_get_config(cfg))
     return runner.run_meltano(command)
 
 
-def meltano_run_import(cfg: dict = {}) -> Dict[str, Any]:
-    return meltano_run("run import", cfg)
-
-
-def dbt_run(tag: str, cfg: dict = {}) -> Dict[str, Any]:
+def dbt_run(tag: str, cfg: dict | PipelineConfig = {}) -> Dict[str, Any]:
     """
     Prefect task wrapper for PipelineRunner.run_dbt.
     """
-    runner = PipelineRunner(PipelineConfig(**cfg))
+    runner = PipelineRunner(_get_config(cfg))
     return runner.run_dbt(tag)
 
 
-def dbt_run_staging(cfg: dict = {}) -> Dict[str, Any]:
+def meltano_run_import(cfg: dict | PipelineConfig = {}) -> Dict[str, Any]:
+    return meltano_run("run import", cfg)
+
+
+def dbt_run_staging(cfg: dict | PipelineConfig = {}) -> Dict[str, Any]:
     return dbt_run("staging", cfg)
 
 
-def dbt_run_silver(cfg: dict = {}) -> Dict[str, Any]:
+def dbt_run_silver(cfg: dict | PipelineConfig = {}) -> Dict[str, Any]:
     return dbt_run("silver", cfg)
 
 
-def dbt_run_gold(cfg: dict = {}) -> Dict[str, Any]:
+def dbt_run_gold(cfg: dict | PipelineConfig = {}) -> Dict[str, Any]:
     return dbt_run("gold", cfg)
 
 
-def dbt_run_tests(cfg: dict = {}) -> Dict[str, Any]:
+def dbt_run_tests(cfg: dict | PipelineConfig = {}) -> Dict[str, Any]:
     return dbt_run("test", cfg)
